36489dc0d1b41c84f7535f60d04e39beb9ef78b2
RightL <liu20001021@hotmail.com>
2015-05-11 14:06:42 +0800

    修改:         ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp 修改:         ConsoleApplication6/ConsoleApplication6/stdafx.cpp 修改:         ConsoleApplication6/ConsoleApplication6/stdafx.h

--- ConsoleApplication6/ConsoleApplication6/stdafx.h
+++ ConsoleApplication6/ConsoleApplication6/stdafx.h
@@ -1,13 +1,7 @@
-// stdafx.h : ��׼ϵͳ�����ļ��İ����ļ���
-// ���Ǿ���ʹ�õ��������ĵ�
-// �ض�����Ŀ�İ����ļ�
-//
-
 #pragma once
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <tchar.h>
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
@@ -17,5 +11,3 @@
 #define X_MAX 15
 #define Y_MAX 15
 #define TIME_DELAY 1
-
-// TODO:  �ڴ˴����ó�����Ҫ������ͷ�ļ�
