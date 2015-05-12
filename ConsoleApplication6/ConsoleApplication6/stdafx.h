8c45dfaf858cc74d76e9741496dfc2a73edc0a3e
LEOCarrow <leeziao@hotmail.com>
2015-05-12 18:19:01 +0800

    H
    HH

--- ConsoleApplication6/ConsoleApplication6/stdafx.h
+++ ConsoleApplication6/ConsoleApplication6/stdafx.h
@@ -1,8 +1,3 @@
-// stdafx.h : ��׼ϵͳ�����ļ��İ����ļ���
-// ���Ǿ���ʹ�õ��������ĵ�
-// �ض�����Ŀ�İ����ļ�
-//
-
 #pragma once
 
 #include <stdlib.h>
@@ -12,10 +7,8 @@
 #include <conio.h>
 #include <Windows.h>
 #include <time.h>
-#include <stdbool.h>
 
 #define X_MAX 15
 #define Y_MAX 15
 #define TIME_DELAY 1
 
-// TODO:  �ڴ˴����ó�����Ҫ������ͷ�ļ�
