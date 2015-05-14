33ba353235199512d71c165e58640bb56a68cb44
RightL <liu20001021@hotmail.com>
2015-05-14 14:06:48 +0800

    修改:         ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -25,16 +25,9 @@ struct Coordinate gen_fruit(const NODE *first);
 
 int main()
 {
-<<<<<<< HEAD
 	int Nowtime, Pretime,index;
 	struct Coordinate fruit = {0, 0};
 	struct NODE *pHead = NULL, *pEnd = NULL;
-=======
-	int Nowtime, Pretime;
-	Present fruit;
-	fruit.x = 0; fruit.y = 0;
-	NODE *pHead = NULL,*pEnd = NULL;
->>>>>>> upstream/master
 	if (!(pHead = settings())) {
 		printf("Initialization Faild\n");
 		exit(-1);
